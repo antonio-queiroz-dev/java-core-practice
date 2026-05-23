524bb06ddbb0e4e6999e5c742f2a8b2393efc961
Antonio Queiroz <antonioqueiroz.dev@gmail.com>
2026-05-23 11:44:47 -0300

    feat: Hashmap extras e27 agrupar palavras por letra inicial

---
+++ src/d_hashmap/extras/e27/AgruparPalavrasPorLetraInicial.java
@@ -0,0 +1,9 @@
+package d_hashmap.extras.e27;
+
+public class AgruparPalavrasPorLetraInicial {
+    public static void main(String[] args) {
+        String frase = "ana ama arroz bolo bala café carne";
+
+
+    }
+}
