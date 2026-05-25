b30c0c9879eb1d51aa6c8e4446de10d44a4fcb01
Antonio Queiroz <antonioqueiroz.dev@gmail.com>
2026-05-25 12:28:04 -0300

    feat: Hashmap extras e27 agrupar palavras por letra inicial

--- src/d_hashmap/extras/e27/AgruparPalavrasPorLetraInicial.java
+++ src/d_hashmap/extras/e27/AgruparPalavrasPorLetraInicial.java
@@ -1,9 +1,30 @@
 package d_hashmap.extras.e27;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class AgruparPalavrasPorLetraInicial {
-    public static void main(String[] args) {
+    static void main() {
         String frase = "ana ama arroz bolo bala café carne";
 
+        String[] palavras = frase.split(" ");
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String p: palavras) {
+            String chave = String.valueOf(p.charAt(0));
+
+            if (map.containsKey(chave)) {
+                map.get(chave).add(p);
+            } else {
+                List<String> nomes = new ArrayList<>();
+                nomes.add(p);
+                map.put(chave,nomes);
+            }
+        }
+
+        System.out.println(map);
 
     }
 }
